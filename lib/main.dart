6a5173ac7270f0251760e23867246c85f4e54110
Hazemhamdy <hazemhamdy1215@gmail.com>
2024-07-12 21:50:07 +0300

    add assets and fonts in pubspec

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:spotify/core/configs/theme/app_theme.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,9 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: AppTheme.lightTheme,
       home: Container(),
     );
   }
